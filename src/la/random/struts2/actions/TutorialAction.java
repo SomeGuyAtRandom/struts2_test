e81ed781765cf08ad4e7e48dbf9602fd3c6c8311
ray <oracle@rayandrades.com>
2014-11-26 13:29:21 -0800

    Simple action mapping - end of tutorial 4

--- src/la/random/struts2/actions/TutorialAction.java
+++ src/la/random/struts2/actions/TutorialAction.java
@@ -1,5 +1,17 @@
 package la.random.struts2.actions;
 
 public class TutorialAction {
+	
+	
+	public TutorialAction(){
+		System.out.println("TutorialAction()");
+	}
+
+	// Required by Struts2
+	public String execute(){
+		System.out.println("TutorialAction.execute()");
+		return "error";
+	}
+	
 
 }
